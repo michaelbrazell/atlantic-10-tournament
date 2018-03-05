efd05dd3e4728efac2f0c223324825bc8e77c09b
Mike Murray <michaelbrazell@gmail.com>
2018-03-05 18:58:13 -0500

    Initial commit with React App Framework setup, good to pull to start fresh project

---
+++ src/components/Brackets.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Brackets extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="mb-3">Brackets</h2>
+      </div>
+    );
+  }
+}
+
+export default Brackets;
