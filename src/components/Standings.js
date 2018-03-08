496855f03ab6993a770a00370e0cf1502ffad811
Mike Murray <michaelbrazell@gmail.com>
2018-03-08 17:56:30 -0500

    Update standings, add win-loss to standings WIP

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -10,38 +10,37 @@ class Brackets extends Component {
             <tr>
               <th scope="col">#</th>
               <th scope="col">Name</th>
-              <th scope="col">Max Points</th>
               <th scope="col">Points</th>
+              <th>Win-Loss</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <th scope="row">1</th>
               <td>John</td>
-              <td>0</td>
-              <td>0</td>
+              <td>11</td>
+              <td>WIP</td>
             </tr>
             <tr>
               <th scope="row">2</th>
               <td>Kevin</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Chris</td>
-              <td>0</td>
-              <td>0</td>
+              <td>8</td>
+              <td>WIP</td>
             </tr>
             <tr>
               <th scope="row">4</th>
               <td>Mike</td>
-              <td>0</td>
-              <td>0</td>
+              <td>8</td>
+              <td>WIP</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Chris</td>
+              <td>5</td>
+              <td>WIP</td>
             </tr>
           </tbody>
         </table>
-
       </div>
     );
   }
